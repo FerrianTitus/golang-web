9911061b1d34fcb7b435b96641243b2d5cdd4f57
Ferrian <ferrian00@gmail.com>
2022-12-05 08:38:31 +0700

    Form Post Test

---
+++ form_post_test.go
@@ -0,0 +1,40 @@
+package golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func FormPost(writer http.ResponseWriter, request *http.Request) {
+	// Parse first
+	err := request.ParseForm()
+	if err != nil {
+		panic(err)
+	}
+
+	// request.PostFormValue("first_name")
+
+	firstName := request.PostForm.Get("first_name")
+	lastName := request.PostForm.Get("last_name")
+
+	fmt.Fprintf(writer, "Hello %s %s", firstName, lastName)
+}
+
+func TestFormPost(t *testing.T) {
+	requstBody := strings.NewReader("first_name=Ferrian&last_name=Titus")
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8081", requstBody)
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	recorder := httptest.NewRecorder()
+
+	FormPost(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
